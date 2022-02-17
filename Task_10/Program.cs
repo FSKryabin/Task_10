339343a7bbd27844b34d96bd2c8a0291dd1d2528
FSKryabin <95165120+FSKryabin@users.noreply.github.com>
2022-02-17 13:36:08 +0300

    Добавьте файлы проекта.

---
+++ Task_10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine("Введите данные");
+            Console.WriteLine("Введите количество градусов угла");
+            int Grade_ = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество минут угла");
+            int Minutes_ = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество секунд угла");
+            int Seconds_ = Convert.ToInt32(Console.ReadLine());
+            Radians radians = new Radians(Grade_, Minutes_, Seconds_);
+            double Radians = radians.ToRadians();
+            Console.WriteLine("Значение данного угла в радианах=" + Radians);
+            Console.ReadKey();
+        }
+
+        
+           
+   
+    }
+}
---
+++ Task_10/Radians.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10
+{
+    class Radians
+    {
+        int Grade;
+        int Minutes;
+        int Seconds;
+
+        public int grade
+        {
+            get
+            {
+                return Grade;
+            }
+            set
+            {
+                Grade = value % 360;
+            }
+
+        }
+
+        public int minutes
+        {
+            get
+            {
+                return Minutes;
+            }
+            set
+            {
+                grade += value / 60;
+                Minutes = value % 60;
+            }
+
+        }
+        public int seconds
+        {
+            get
+            {
+                return Seconds;
+            }
+            set
+            {
+                minutes += value / 60;
+                Seconds = value % 60;
+            }
+
+        }
+
+        public Radians(int Grade, int Minutes, int Seconds)
+        {
+            this.Grade = Grade;
+            this.Minutes = Minutes;
+            this.Seconds = Seconds;
+
+        }
+         
+        public double ToRadians()
+        {
+            return (Grade + Minutes / 60 + Seconds / 3600) * (Math.PI) / 180;
+        }
+    }
+}
